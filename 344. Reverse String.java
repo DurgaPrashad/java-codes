3272ed62aa28249b80deeda49cba655608ef5bbd
Durga Prashad <120201162+DurgaPrashad@users.noreply.github.com>
2024-06-02 20:17:32 +0530

    Create 344. Reverse String.java

---
+++ 344. Reverse String.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+class Solution {
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        char[] s1 = {'h', 'e', 'l', 'l', 'o'};
+        solution.reverseString(s1);
+        System.out.println(Arrays.toString(s1)); 
+
+        char[] s2 = {'H', 'a', 'n', 'n', 'a', 'h'};
+        solution.reverseString(s2);
+        System.out.println(Arrays.toString(s2)); //dp
+    }
+}
