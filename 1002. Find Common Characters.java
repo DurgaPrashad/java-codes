37eb7fa5e2bad59b18539bd010fc59ece42dc6f1
Durga Prashad <120201162+DurgaPrashad@users.noreply.github.com>
2024-06-05 20:17:08 +0530

    Create 1002. Find Common Characters.java

---
+++ 1002. Find Common Characters.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<String> commonChars(String[] words) {
+        int[] minFreq = new int[26];
+        for (int i = 0; i < 26; i++) {
+            minFreq[i] = Integer.MAX_VALUE;
+        }
+        
+        for (String word : words) {
+            int[] charFreq = new int[26];
+            for (char c : word.toCharArray()) {
+                charFreq[c - 'a']++;
+            }
+            
+            for (int i = 0; i < 26; i++) {
+                minFreq[i] = Math.min(minFreq[i], charFreq[i]);
+            }
+        }
+        
+        List<String> result = new ArrayList<>();
+        for (int i = 0; i < 26; i++) {
+            for (int j = 0; j < minFreq[i]; j++) {
+                result.add(Character.toString((char) (i + 'a')));
+            }
+        }
+        
+        return result;
+    }
+}
