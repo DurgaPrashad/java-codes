4cbd1cf6d28f1145e5fde6eb0ea0faab56efbb95
Durga Prashad <120201162+DurgaPrashad@users.noreply.github.com>
2024-07-02 17:52:44 +0530

    Create 350. Intersection of Two Arrays II.java

---
+++ 350. Intersection of Two Arrays II.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> countMap = new HashMap<>();
+
+       
+        for (int num : nums1) {
+            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+        }
+
+        List<Integer> intersection = new ArrayList<>();
+
+
+        for (int num : nums2) {
+            if (countMap.containsKey(num) && countMap.get(num) > 0) {
+                intersection.add(num);
+                countMap.put(num, countMap.get(num) - 1);
+            }
+        }
+
+        int[] result = new int[intersection.size()];
+        for (int i = 0; i < intersection.size(); i++) {
+            result[i] = intersection.get(i);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums1 = {1, 2, 2, 1};
+        int[] nums2 = {2, 2};
+        int[] result = solution.intersect(nums1, nums2);
+
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
+    }
+}
